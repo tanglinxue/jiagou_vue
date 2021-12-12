b5f81fa72be1199c32bb09391f2513aa1dc46c30
tanglinxuework@163.com <tanglinxuework@163.com>
2021-12-12 21:59:09 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // Vue的核心代码,只是Vue的一个声明
+import {initMixin} from './init'
 function Vue(options){
     //进行vue的初始化操作
-    console.log(options)
     this._init(options) 
 }
 
+// 通过引入文件的方式，给Vue原型上添加方法
+initMixin(Vue)
 export default Vue
---
+++ src/init.js
@@ -0,0 +1,7 @@
+
+// 在原型上添加一个Init方法
+export function initMixin(Vue){
+    Vue.prototype._init=function(){
+
+    }
+}
