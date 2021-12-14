5047e0151b2b1f6f306d1621542432023ad5f371
tanglinxue <31237473+tanglinxue@users.noreply.github.com>
2021-12-14 17:33:11 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Vue的核心代码,只是Vue的一个声明
-import {initMixin} from './init'
-function Vue(options){
-    //进行vue的初始化操作
-    this._init(options) 
+import { initMixin } from './init'
+function Vue(options) {
+  //进行vue的初始化操作
+  this._init(options)
 }
 
 // 通过引入文件的方式，给Vue原型上添加方法
--- src/init.js
+++ src/init.js
@@ -1,7 +1,10 @@
 
 // 在原型上添加一个Init方法
-export function initMixin(Vue){
-    Vue.prototype._init=function(){
-
-    }
+export function initMixin(Vue) {
+  // 初始化流程
+  Vue.prototype._init = function (options) {
+    console.log(options)
+    // 数据的劫持
+    const vm = this
+  }
 }
